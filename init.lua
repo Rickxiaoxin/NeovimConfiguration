6dc9c3f1771a064b8d9acf2cf1226c3054ea0500
WeiShx <2642138150@qq.com>
2025-08-15 16:51:29 +0800

    使用basedpyright作为python的LSP

--- init.lua
+++ init.lua
@@ -24,9 +24,9 @@ vim.o.relativenumber = true
 -- increase startup-time. Remove this option if you want your OS clipboard to remain independent.
 -- See `:help 'clipboard'`
 vim.api.nvim_create_autocmd('UIEnter', {
-	callback = function()
-		vim.o.clipboard = 'unnamedplus'
-	end,
+    callback = function()
+	vim.o.clipboard = 'unnamedplus'
+    end,
 })
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
@@ -67,10 +67,10 @@ vim.keymap.set({ 'n' }, '<A-l>', '<C-w>l')
 -- Highlight when yanking (copying) text.
 -- Try it with `yap` in normal mode. See `:h vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
-	desc = 'Highlight when yanking (copying) text',
-	callback = function()
-		vim.hl.on_yank()
-	end,
+    desc = 'Highlight when yanking (copying) text',
+    callback = function()
+	vim.hl.on_yank()
+    end,
 })
 
 -- [[ Create user commands ]]
@@ -78,9 +78,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- Create a command `:GitBlameLine` that print the git blame for the current line
 vim.api.nvim_create_user_command('GitBlameLine', function()
-	local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
-	local filename = vim.api.nvim_buf_get_name(0)
-	print(vim.fn.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }))
+    local line_number = vim.fn.line('.') -- Get the current line number. See `:h line()`
+    local filename = vim.api.nvim_buf_get_name(0)
+    print(vim.fn.system({ 'git', 'blame', '-L', line_number .. ',+1', filename }))
 end, { desc = 'Print the git blame for the current line' })
 
 -- [[ Add optional packages ]]
@@ -92,81 +92,64 @@ end, { desc = 'Print the git blame for the current line' })
 vim.cmd('packadd! nohlsearch')
 
 -- [[ LSP Configuration ]]
-vim.lsp.enable({ 'lua_ls', 'pyright', 'clangd', 'texlab' })
+vim.lsp.enable({ 'lua_ls', 'basedpyright', 'clangd', 'texlab' })
 
 vim.cmd [[set completeopt+=menuone,noselect,popup]]
 vim.api.nvim_create_autocmd('LspAttach', {
-	group = vim.api.nvim_create_augroup('MyLsp', {}),
-	callback = function(args)
-		local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
-		-- if client:supports_method('textDocument/implementation') then
-		--   -- Create a keymap for vim.lsp.buf.implementation ...
-		-- end
-
-		-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
-		if client:supports_method('textDocument/completion') then
-			-- Optional: trigger autocompletion on EVERY keypress. May be slow!
-			-- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
-			-- client.server_capabilities.completionProvider.triggerCharacters = chars
-
-			vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
-		end
-
-		-- Auto-format ("lint") on save.
-		-- Usually not needed if server supports "textDocument/willSaveWaitUntil".
-		if not client:supports_method('textDocument/willSaveWaitUntil')
-			and client:supports_method('textDocument/formatting') then
-			vim.api.nvim_create_autocmd('BufWritePre', {
-				group = vim.api.nvim_create_augroup('MyLsp', { clear = false }),
-				buffer = args.buf,
-				callback = function()
-					vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
-				end,
-			})
-		end
-	end,
+    group = vim.api.nvim_create_augroup('MyLsp', {}),
+    callback = function(args)
+	local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+	-- Enable auto-completion. Note: Use CTRL-Y to select an item. |complete_CTRL-Y|
+	if client:supports_method('textDocument/completion') then
+	    -- Optional: trigger autocompletion on EVERY keypress. May be slow!
+	    -- local chars = {}; for i = 32, 126 do table.insert(chars, string.char(i)) end
+	    -- client.server_capabilities.completionProvider.triggerCharacters = chars
+	    vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+	end
+    end,
 })
 
 require 'nvim-treesitter.configs'.setup {
-	-- A list of parser names, or "all" (the listed parsers MUST always be installed)
-	ensure_installed = { "cpp", "python" },
+    -- A list of parser names, or "all" (the listed parsers MUST always be installed)
+    ensure_installed = { "cpp", "python" },
 
-	-- Install parsers synchronously (only applied to `ensure_installed`)
-	sync_install = false,
+    -- Install parsers synchronously (only applied to `ensure_installed`)
+    sync_install = false,
 
-	-- Automatically install missing parsers when entering buffer
-	-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-	auto_install = false,
+    -- Automatically install missing parsers when entering buffer
+    -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+    auto_install = false,
 
-	-- List of parsers to ignore installing (or "all")
-	-- ignore_install = { "javascript" },
+    -- List of parsers to ignore installing (or "all")
+    -- ignore_install = { "javascript" },
 
-	---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
-	-- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+    ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+    -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
-	highlight = {
-		enable = true,
+    highlight = {
+	enable = true,
 
-		-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-		-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-		-- the name of the parser)
-		-- list of language that will be disabled
-		-- disable = { "c", "rust" },
-		-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
-		disable = function(_, buf)
-			local max_filesize = 100 * 1024 -- 100 KB
-			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-			if ok and stats and stats.size > max_filesize then
-				return true
-			end
-		end,
+	-- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
+	-- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
+	-- the name of the parser)
+	-- list of language that will be disabled
+	-- disable = { "c", "rust" },
+	-- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+	disable = function(_, buf)
+	    local max_filesize = 100 * 1024 -- 100 KB
+	    local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+	    if ok and stats and stats.size > max_filesize then
+		return true
+	    end
+	end,
 
-		-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-		-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-		-- Using this option may slow down your editor, and you may see some duplicate highlights.
-		-- Instead of true it can also be a list of languages
-		additional_vim_regex_highlighting = false,
-	},
+	-- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+	-- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+	-- Using this option may slow down your editor, and you may see some duplicate highlights.
+	-- Instead of true it can also be a list of languages
+	additional_vim_regex_highlighting = false,
+    },
 }
 
 -- [[ Vimtex configuration ]]
+
